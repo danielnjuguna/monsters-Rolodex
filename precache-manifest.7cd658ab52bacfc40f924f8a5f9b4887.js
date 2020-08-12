d8cc6169ba38b7b74133a1522805f7f575cbf3c8
danielnjuguna <danielnjuguna9042@gmail.com>
2020-08-12 07:58:59 -0700

    Updates

---
+++ precache-manifest.7cd658ab52bacfc40f924f8a5f9b4887.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af9ae6feef8b1ad32ed365300ef82fe3",
+    "url": "/monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "f2a49c42a8b7939e9129",
+    "url": "/monsters-Rolodex/static/css/main.92036739.chunk.css"
+  },
+  {
+    "revision": "e26f3b49cda25c8c1e51",
+    "url": "/monsters-Rolodex/static/js/2.36960f10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-Rolodex/static/js/2.36960f10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2a49c42a8b7939e9129",
+    "url": "/monsters-Rolodex/static/js/main.d7e2ab04.chunk.js"
+  },
+  {
+    "revision": "8cf93566278536adc497",
+    "url": "/monsters-Rolodex/static/js/runtime-main.f7b07367.js"
+  }
+]);
